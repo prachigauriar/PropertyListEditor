de181daae76e63dbd4eb65fc72f0300f2a31ef92
Prachi Gauriar <prachi.gauriar@ticketmaster.com>
2015-07-01 13:24:40 -0400

    Initial Commit

---
+++ PListEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PListEditor
+//
+//  Created by Prachi Gauriar on 7/1/2015.
+//  Copyright © 2015 Quantum Lens Cap. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
